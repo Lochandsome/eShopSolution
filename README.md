fde6d0683b701d699d21e597cca06fd6dba436b6
PhungThanhLoc <Phungthanhloc.2000@gmail.com>
2021-03-13 11:32:07 +0700

    feature/products_apis

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Microsoft.EntityFrameworkCore.Design
 ## technologies
 ## how to configure  and run
 ## how to contribute
+
+## add Swagger
+
+## Domian liên quan tới nghiệp vụ
+## Host chứa các cái ứng dụng web
+## Infrastructure  chứa main kết nối sql
--- eShopSolution.Application/Catalog/Products/IPublicProductService.cs
+++ eShopSolution.Application/Catalog/Products/IPublicProductService.cs
@@ -8,8 +8,7 @@ namespace eShopSolution.Application.Catalog.Products
     public interface IPublicProductService
     {
         // phần này sẽ chỉ chứa phương thức cho phần bên ngoài khách hàng đọc
-        Task<PagedResult<ProductViewModel>> GetAllByCategoryId(GetPublicProductPagingRequest request);
+        Task<PagedResult<ProductViewModel>> GetAllByCategoryId(string languageId, GetPublicProductPagingRequest request);
 
-        Task<List<ProductViewModel>> GetAll();
     }
 }
