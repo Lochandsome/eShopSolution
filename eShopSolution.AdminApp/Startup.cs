45da3c27a4b3a7c65c2c6411db263f4364c2d1c4
PhungThanhLoc <Phungthanhloc.2000@gmail.com>
2021-03-23 22:39:26 +0700

    unit 35, complete language installation

--- eShopSolution.AdminApp/Startup.cs
+++ eShopSolution.AdminApp/Startup.cs
@@ -45,8 +45,8 @@ namespace eShopSolution.AdminApp
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             services.AddTransient<IUserApiClient, UserApiClient>();
-
             services.AddTransient<IRoleApiClient, RoleApiClient>();
+            services.AddTransient<ILanguageApiClient, LanguageApiClient>();
 
             IMvcBuilder builder = services.AddRazorPages();
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
